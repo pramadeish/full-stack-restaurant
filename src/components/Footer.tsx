5de0495d3fb8cc1c6c2cdebf4a450e98c068dea4
pramadeish <pramadeishsm@gmail.com>
2023-08-11 15:15:23 +0530

    initial commit with ui

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="h-12 md:h-24 px-4 lg:px-10 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">SMP-RES</Link>
+      <p>@ALL RIGHTS RESERVED.</p>
+    </div>
+  )
+}
+
+export default Footer
