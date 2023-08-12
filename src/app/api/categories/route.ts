a2f3229935bfd4292b16876b88bbc3069aa8d786
pramadeish <pramadeishsm@gmail.com>
2023-08-12 21:43:36 +0530

    migrated models and fetching of categories

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+const prisma = new PrismaClient()
+//Fetch all categories
+export const GET = async () => {
+  try {
+      const categories=await prisma.category.findMany()
+      return new NextResponse(
+        JSON.stringify(categories),
+        { status: 200 }
+      );
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
+export const POST = () => {
+  return new NextResponse("HELLO", { status: 200 });
+};
