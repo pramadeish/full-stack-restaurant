a945da78d128328436fee0d8abac35c9406e83dd
pramadeish <pramadeishsm@gmail.com>
2023-08-15 01:10:25 +0530

    added user model
    schema for account, session ,user and verification token models and migrated using prisma

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,10 @@
-import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
+import { prisma } from "./connect";
 
 export const authOptions:NextAuthOptions={
+  adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
           clientId: process.env.GOOGLE_ID as string,
@@ -10,3 +13,5 @@ export const authOptions:NextAuthOptions={
         // ...add more providers here
       ],
 }
+
+export const getAuthSession = ()=>getServerSession(authOptions)
