d51f4bef5535d5d712369c67b3e47327f5e2ecee
pramadeish <pramadeishsm@gmail.com>
2023-09-01 12:20:29 +0530

    Delete product(Only admin)

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DeleteButton from '@/components/DeleteButton';
 import Price from '@/components/Price'
 import { singleProduct } from '@/data'
 import { ProductType } from '@/types/types';
@@ -24,7 +25,7 @@ type Props={
 const SingleProductPage = async ({params}:{params:{id:string}}) => {
   const singleProduct: ProductType=await getData(params.id)
   return (
-    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center'>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center relative'>
         {/* Image container */}
       {singleProduct.img && (<div className='relative w-full h-1/2 md:h-[70%]'>
         <Image src={singleProduct.img} alt="" className='object-contain' fill/>
@@ -35,6 +36,7 @@ const SingleProductPage = async ({params}:{params:{id:string}}) => {
         <p>{singleProduct.desc}</p>
         <Price product={singleProduct}/>
       </div>
+      <DeleteButton id={singleProduct.id}/>
     </div>
   )
 }
